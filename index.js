1f1689e281c0ab8dc45fac4f2d3134fed783c9d9
Michael Carter <mtcarter010@gmail.com>
2019-05-15 23:27:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  
+  const output = [];
   for (let counter = 0; counter < musicians.length; counter++){
-    
+    output.push(musician[counter] + " plays " + instruments[0])
   }
 }
